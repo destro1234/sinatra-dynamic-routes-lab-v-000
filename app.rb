8bd674872960cdaf584e41ac36a8e762cadeba03
Elias Ventura <eliasventura23@gmail.com>
2019-02-28 16:53:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,8 +25,7 @@ class App < Sinatra::Base
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
     operation = params[:operation]
-
-    #num1.send(operation, num2)
-    num1.send(operation ,num2)
+  when operation == "add"
+    "#{num1} + #{num2}"
   end
 end
