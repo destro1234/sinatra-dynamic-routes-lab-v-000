279c00b41a02e84198833b3c026e3390eb72e060
Elias Ventura <eliasventura23@gmail.com>
2019-02-28 16:58:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ class App < Sinatra::Base
     num2 = params[:number2].to_i
     operation = params[:operation]
   if operation == "add"
-    "#{num1} + #{num2}"
+    eval"#{num1} + #{num2}"
   end
   end
 end
