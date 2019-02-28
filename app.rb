f1f2501ce48d179dc42be9da6fb0a9519fcdd7f5
Elias Ventura <eliasventura23@gmail.com>
2019-02-28 17:08:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,8 +22,15 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    num1 = params[:number1]
-    num2 = params[:number2]
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+
     operation = params[:operation]
+
+    case operation
+    when "add"
+      "#{num1}" + "#{num2}"
+    end
+    
   end
 end
